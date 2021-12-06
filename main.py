9f5f826cccf386890d79037aeeb265db054d5bf9
Alex Seregin <alexeusgr@yahoo.com>
2021-12-06 22:01:19 +0300

    ta-lib, pandas-ta

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 #import pylab
 from  data.sources.market import moex, binance 
 import pandas_datareader as pdr
+import pandas_ta as ta
+import talib
 
 dk_discount = "IBM, ED, BEN, AFL, BDX, ADM, MMM, WBA, CAH, ABBV, SWK, ATO, NUE".split(sep = ', ')
 #import
@@ -16,6 +18,11 @@ dk_discount = "IBM, ED, BEN, AFL, BDX, ADM, MMM, WBA, CAH, ABBV, SWK, ATO, NUE".
 # Get data, define the investment universe, design factors, combine factors, optimize portfolio, exec orders. /Jensen
 
 # momentum, sentiment, value, volatility, size, quality
-import pandas_ta as ta
 df = pd.DataFrame()
-df = df.ta.ticker(dk_discount[0])
+df = df.ta.ticker(dk_discount[0], period = '1y', interval = '1wk')
+df = df.dropna()
+output = talib.SMA(df.Close)
+
+df.Close.plot()
+output.plot()
+plt.show()
