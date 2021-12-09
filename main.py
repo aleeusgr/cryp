20cc58c9349d1dcc031d7d590cfb4ac9f5e553e7
Alex Seregin <alexeusgr@yahoo.com>
2021-12-09 22:31:08 +0300

    read the dataset

--- main.py
+++ main.py
@@ -7,23 +7,8 @@ import matplotlib.pyplot as plt
 import pandas_datareader as pdr
 #import pandas_ta as ta
 import talib
-from utility import timer_func
+from utility import *
 
 dk_discount = "IBM, ED, BEN, AFL, BDX, ADM, MMM, WBA, CAH, ABBV, SWK, ATO, NUE".split(sep = ', ')
-DATA_STORE = '~/workshop/machine-learning-for-trading/data/assets.h5'
-START = 2011
-END = 2018
-idx =pd.IndexSlice
-  
 
-@timer_func
-def read_h5():
-    # exec time 39s from HDD, 25s from SSD
-    with pd.HDFStore(DATA_STORE) as store:
-        #prices = (store['quandl/wiki/prices'].loc[idx[str(START):str(END), :], 'adj_close'].unstack('ticker'))
-        prices = None
-        stocks = store['us_equities/stocks'].loc[dk_discount]# ['marketcap', 'ipoyear', 'sector']]
-    return prices,stocks
-
-
-prices, stocks = read_h5()
+df = read_h5()
--- utility.py
+++ utility.py
@@ -12,3 +12,21 @@ def timer_func(func):
         print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
         return result
     return wrap_func
+
+
+@timer_func
+def read_h5(START = 2011,END = 2018):
+    import pandas as pd
+    DATA_STORE = '~/workshop/machine-learning-for-trading/data/assets.h5'
+    idx =pd.IndexSlice
+    with pd.HDFStore(DATA_STORE) as store:
+        print(store.info()) 
+        #result = (store['quandl/wiki/prices'].loc[idx[str(START):str(END), :], 'adj_close'].unstack('ticker')) # exec time 39s from HDD, 25s from SSD
+        #result = (store['engineered_features']) # time = 2.5s 
+        #result = (store['quandl/wiki/stocks']) # is in us_equities/stocks
+        #result = (store['sp500/fred']) #???
+
+        # company info: age, market cap, HQ location, etc. 
+        result = (store['sp500/stocks']) # seems better then us_equities/stocks, but no market cap
+        #result = store['us_equities/stocks']# ['marketcap', 'ipoyear', 'sector']] #time = 1s
+    return  result
