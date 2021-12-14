e2741a1208c6f5fcdbc4e60d5a18f6ad005fac33
Alex Seregin <alexeusgr@yahoo.com>
2021-12-14 09:11:18 +0300

    fama-french regression

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from utility import *
 dk_discount = "IBM, ED, BEN, AFL, BDX, ADM, MMM, WBA, CAH, ABBV, SWK, ATO, NUE".split(sep = ', ')
 #stock = Asset(dk_discount[0])
 
-df = read_h5()
+#df = read_h5()
 fama_french = ff5()
 
 #price data
@@ -21,3 +21,11 @@ symbol = dk_discount[0]
 start, end = (fama_french.index[i].strftime('%Y-%m-%d') for i in(0,-1))
 df = pdr.DataReader(f'{symbol}', 'yahoo', start, end)
 df = df.resample('M').last()
+df['returns'] = df['Adj Close'].pct_change()
+
+X = fama_french[:-1].to_numpy()
+y = df.returns[1:].to_numpy()
+import statsmodels.api as sm
+X_ols = sm.add_constant(X)
+model = sm.OLS(y, X_ols).fit()
+print(model.summary())
